5906eda76f373fe5cd9fc65efd02c4b0add92077
h-r-wells2 <hrwells10246@gmail.com>
2023-08-18 03:29:43 +0530

    layout update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,21 +1,21 @@
-import { Roboto_Mono } from "next/font/google";
+import { Roboto_Slab } from "next/font/google";
 
-const RoboMono = Roboto_Mono({ subsets: ["latin"] });
+const RoboMono = Roboto_Slab({ subsets: ["latin"] });
 
 const Hero = () => {
   return (
-    <div className="bg-transparent flex flex-col max-w-3xl mx-auto my-32 text-white w-full">
+    <div className="bg-transparent flex flex-col max-w-7xl mx-auto my-32 text-white w-full px-3">
       <div>
         <span className="text-xl">Hello, My name is</span>
         <h1
-          className={`text-4xl text-teal-300 font-semibold ${RoboMono.className}`}
+          className={`text-[50px] sm:text-[100px] text-teal-300 font-semibold ${RoboMono.className}`}
         >
-          Shubham <span className="text-teal-300">Kadam...</span>{" "}
+          Shubham Kadam
         </h1>
       </div>
-      <div className="mt-10 text-orange-300 text-3xl font-semibold">
+      <div className="mt-10 text-orange-300 text-xl sm:text-3xl font-semibold">
         <h1>Web Developer</h1>
-        <h2 className="text-white">I code therefore I am...occasionally functional.</h2>
+        <h2 className="text-white font-mono ">I code therefore I am...occasionally functional.</h2>
       </div>
     </div>
   );
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,39 @@
+import { SocialIcon } from "react-social-icons";
+
 const Navbar = () => {
   return (
-    <div className="flex py-5 px-3 bg-secondary/60 w-full">
-      <div className="w-full flex justify-between">
-        <h1 className="font-bold text-3xl text-white">
-          HRWells <span className="text-teal-300">Blog</span>
-        </h1>
-        <div className="flex gap-4 mx-4 text-white text-xl font-semibold">
+    <header className="flex py-5 px-3 bg-secondary/20 w-full">
+      <div className="w-full flex justify-between max-w-[1400px] mx-auto">
+        <div className="flex items-center">
+          <h1 className="font-bold text-3xl text-white cursor-pointer hidden sm:block">
+            HR<span className="text-teal-300">Wells</span>
+          </h1>
+          <div className="border-orange-500 border-x h-10 w-2 grow justify-center min-w-md ml-5 hidden sm:flex"/>
+          <div className=" flex w-fit sm:ml-4">
+            <SocialIcon
+              url="https://www.linkedin.com/in/shubham-kadam-0620b722a/"
+              fgColor="#5EEAD4"
+              bgColor="transparent"
+            />
+            <SocialIcon
+              url="https://github.com/h-r-wells2"
+              fgColor="#5EEAD4"
+              bgColor="transparent"
+            />
+            <SocialIcon
+              url="https://t.me/h_r_wells"
+              fgColor="#5EEAD4"
+              bgColor="transparent"
+            />
+            {/* <SocialIcon url="" fgColor="#5EEAD4" bgColor="transparent" /> */}
+          </div>
+        </div>
+        <div className="flex items-center gap-4 mx-4 text-white text-lg sm:text-xl font-semibold">
           <h2 className="hover:text-teal-300 cursor-pointer">Projects</h2>
           <h2 className="hover:text-teal-300 cursor-pointer">Contact</h2>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
